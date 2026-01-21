fd82b581b70ae04c529733ed83c0e59ae314bfb2
Robson Clayton <clayton.netvision@gmail.com>
2026-01-21 10:07:19 -0300

    Feat: Add PDF import system for automatic workout creation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { Router, Route } from 'wouter';
-import { ThemeProvider } from '@/components/theme-provider';
 import Home from '@/pages/Home';
 import Editor from '@/pages/Editor';
 import Display from '@/pages/Display';
@@ -8,14 +7,12 @@ import PDFImport from '@/pages/PDFImport';
 
 export default function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <Route path="/" component={Home} />
-        <Route path="/editor" component={Editor} />
-        <Route path="/display" component={Display} />
-        <Route path="/manager" component={WorkoutManager} />
-        <Route path="/import-pdf" component={PDFImport} />
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/editor" component={Editor} />
+      <Route path="/display" component={Display} />
+      <Route path="/manager" component={WorkoutManager} />
+      <Route path="/import-pdf" component={PDFImport} />
+    </Router>
   );
 }
